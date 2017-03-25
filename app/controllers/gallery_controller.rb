10643c39217d7e01888bd661d6dad3341007aaa8
alex <alexander.schnedler@gmail.com>
2017-03-25 13:27:43 -0500

    all galleries working

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -1,15 +1,21 @@
 class GalleryController < ApplicationController
   def people
+    @title = "PEOPLE"
+    @pics = Pic.where(gallery: 'people')
   end
 
   def travel
+    @title = "TRAVEL"
+    @pics = Pic.where(gallery: 'travel')
   end
 
   def night
+    @title = "NIGHT"
     @pics = Pic.where(gallery: 'night')
   end
 
   def adventure
-    @pics = Pic.all
+    @title = "ADVENTURE"
+    @pics = Pic.where(gallery: 'adventure')
   end
 end
